febc110e4f4a18cb6890eeca9542a482f9245602
Benjamin Narum <b_narum@outlook.com>
2021-04-29 08:53:31 +0200

    Flipping lat-lon direction

--- S2S/gridpp_helpers.py
+++ S2S/gridpp_helpers.py
@@ -12,8 +12,8 @@ def make_grid_from_grb(grb_data):
 
 def make_points_from_grb(grb_data, valid_points):
 
-    latlats, lonlons = np.meshgrid(
-        grb_data.latitude.data, grb_data.longitude.data
+    lonlons, latlats = np.meshgrid(
+        grb_data.longitude.data, grb_data.latitude.data, 
     )
 
     grid_object = gridpp.Points(latlats[valid_points], lonlons[valid_points])
